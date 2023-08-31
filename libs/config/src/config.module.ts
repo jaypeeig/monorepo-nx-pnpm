c05b4127852971c579626cd610966e9066e1dcec
Jaypee Ignacio <jignacio@cambridge.org>
2023-08-31 18:20:04 +0800

    Add config lib

---
+++ libs/config/src/config.module.ts
@@ -0,0 +1,18 @@
+import { Module } from "@nestjs/common";
+import { ConfigService } from "./config.service";
+
+const configFactory = {
+  provide: ConfigService,
+  useFactory: () => {
+    const config = new ConfigService();
+    return config;
+  },
+};
+
+@Module({
+  imports: [],
+  controllers: [],
+  providers: [configFactory],
+  exports: [configFactory],
+})
+export class ConfigModule {}
