ca4e869b400400640400ace25fdbc93f99415658
Jaypee Ignacio <jignacio@cambridge.org>
2023-08-31 10:24:54 +0800

    Reorder pages for nuxt

---
+++ apps/nuxt-landing/layouts/default.vue
@@ -0,0 +1,41 @@
+<template>
+  <div>
+    <header>
+      <nav>
+        <NuxtLink to="/">App</NuxtLink>
+        <NuxtLink to="/about">About</NuxtLink>
+      </nav>
+    </header>
+    <slot />
+    <footer>
+      Copyright © {{ currentYear }}
+    </footer>
+  </div>
+</template>
+
+<script setup lang="ts">
+  const currentYear = new Date().getFullYear()
+</script>
+
+<style scoped>
+header {
+  background-color: #333;
+  color: #fff;
+  padding: 10px 0;
+  text-align: center;
+}
+
+footer {
+  background-color: #eee;
+  padding: 10px 0;
+  text-align: center;
+}
+
+nav a {
+  margin: 0 10px;
+  color: #fff;
+  text-decoration: none;
+}
+
+</style>
+
---
+++ apps/nuxt-landing/pages/about.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <h1>About Us</h1>
+    <p>
+      We are a team of passionate individuals committed to providing the best solutions for your needs.
+    </p>
+  </div>
+</template>
+
+<style scoped>
+h1 {
+  color: #4a4a4a;
+  text-align: center;
+}
+p {
+  font-size: 18px;
+  text-align: center;
+  max-width: 600px;
+  margin: 20px auto;
+}
+</style>
